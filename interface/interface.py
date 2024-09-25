a851051741a91c36e3f01d4a3e051e77637d2527
Adil Zafar <adil.khan@primepowered.com>
2024-09-25 13:23:49 -0700

    Add docstrings

--- interface/interface.py
+++ interface/interface.py
@@ -8,7 +8,23 @@ from .interface_ui import Ui_MainWindow
 
 
 class Interface(QMainWindow, Ui_MainWindow):
-    def __init__(self, app_path, *args, **kwargs):
+    RUNTIME_ERROR_TITLE = 'Runtime Error'
+    INPUT_ERROR_TITLE = 'Input Error'
+    DATA_VALIDATION_ERROR_TITLE = 'Data Validation Error'
+    RUNTIME_ERROR_MSG = 'Please select a study.'
+    NO_OPTION_SELECTED_MSG = 'Please select an option to continue.'
+    INVALID_PORT_MSG = 'Please enter a valid ETAP Datahub port number.'
+    INVALID_ETAP_DIR_MSG = 'Please enter a valid ETAP project directory path.'
+    INVALID_OUTPUT_DIR_MSG = 'Please enter a valid output file directory path.'
+
+    def __init__(self, app_path: str, *args, **kwargs):
+        """
+        Initializes the interface with the given application path and optional arguments.
+
+        :param str app_path: Path to the application directory.
+        :param args: Additional positional arguments.
+        :param kwargs: Additional keyword arguments.
+        """
         super(Interface, self).__init__(*args, **kwargs)
         self.icon_path = str(Path(app_path, './res', 'icon.ico'))
         self.setWindowIcon(QIcon(self.icon_path))
@@ -23,21 +39,35 @@ class Interface(QMainWindow, Ui_MainWindow):
         self.include_revisions_input.setVisible(False)
         self.show()
 
-    def show_file_browser_input(self):
+    def show_file_browser_input(self) -> None:
+        """
+        Opens a file dialog to select the ETAP project directory and sets the directory path.
+        """
         file_dialog = QFileDialog().getExistingDirectory()
         self.etap_dir.setText(file_dialog)
 
-    def show_file_browser_output(self):
+    def show_file_browser_output(self) -> None:
+        """
+        Opens a file dialog to select the output directory and sets the directory path.
+        """
         file_dialog = QFileDialog().getExistingDirectory()
         self.output_dir.setText(file_dialog)
 
-    def connect_buttons(self):
+    def connect_buttons(self) -> None:
+        """
+        Connects buttons in the UI to their respective event handlers.
+        """
         self.browse_btn.clicked.connect(self.show_file_browser_input)
         self.browse_btn_2.clicked.connect(self.show_file_browser_output)
         self.generate_btn.clicked.connect(self.execute_worker_thread)
         self.clear_all_btn.clicked.connect(self.clear_all_inputs)
 
-    def set_default_output_dir(self, is_checked):
+    def set_default_output_dir(self, is_checked: bool) -> None:
+        """
+        Sets the default output directory to the ETAP directory if checked, otherwise clears it.
+
+        :param bool is_checked: Whether the option to set the default output directory is checked.
+        """
         if is_checked:
             self.output_dir.setText(self.etap_dir.text())
             self.etap_dir_conn = self.etap_dir.textChanged.connect(self.output_dir.setText)
@@ -45,7 +75,10 @@ class Interface(QMainWindow, Ui_MainWindow):
             self.output_dir.clear()
             self.etap_dir.textChanged.disconnect(self.etap_dir_conn)
 
-    def execute_worker_thread(self):
+    def execute_worker_thread(self) -> None:
+        """
+        Validates inputs and executes either the Device Duty or Arc Flash worker threads.
+        """
         port = self.port.text()
         input_dir_path = Path(self.etap_dir.text())
         output_dir_path = Path(self.output_dir.text())
@@ -82,31 +115,58 @@ class Interface(QMainWindow, Ui_MainWindow):
         else:
             self.run_arc_flash(*arg_list_arc_flash)
 
-    def run_arc_flash(self, *args):
+    def run_arc_flash(self, *args) -> None:
+        """
+        Runs the Arc Flash worker if the Arc Flash checkbox is checked.
+
+        :param args: Arguments to pass to the Arc Flash worker.
+        """
         if self.arc_flash_checkbox.isChecked():
             self.af_worker = ArcFlashWorker(*args)
             self.af_worker.error_occurred.connect(self.handle_error)
             self.af_worker.process_finished.connect(self.handle_finished)
             self.af_worker.start()
 
-    def handle_error(self, text):
+    def handle_error(self, text: str) -> None:
+        """
+        Handles errors by displaying a message box with the given error message.
+
+        :param str text: The error message to display.
+        """
         self.show_message('Runtime Error', text, QMessageBox.Critical)
 
-    def show_message(self, title, message, icon=QMessageBox.Warning):
+    def show_message(self, title: str, message: str, icon: QMessageBox.Icon = QMessageBox.Warning) -> None:
+        """
+        Displays a message box with the given title, message, and icon.
+
+        :param str title: The title of the message box.
+        :param str message: The message to display.
+        :param QMessageBox.Icon icon: The icon to display in the message box.
+        """
         message_box = QMessageBox()
         message_box.setWindowIcon(QIcon(self.icon_path))
         message_box.setIcon(icon)
         message_box.setText(message)
         message_box.setWindowTitle(title)
         message_box.setStandardButtons(QMessageBox.Ok)
-        retval = message_box.exec_()
+        message_box.exec_()
 
     @staticmethod
-    def handle_finished(wb_path):
+    def handle_finished(wb_path: str) -> None:
+        """
+        Handles the completion of a process by opening the file if it exists.
+
+        :param str wb_path: The path to the workbook file to open.
+        """
         if Path(wb_path).is_file():
             os.startfile(wb_path)
 
-    def validate_inputs(self):
+    def validate_inputs(self) -> bool:
+        """
+        Validates the input fields in the UI and displays appropriate error messages.
+
+        :return bool: True if all inputs are valid, False otherwise.
+        """
         port = self.port.text()
         input_dir_path = self.etap_dir.text()
         output_dir_path = self.output_dir.text()
@@ -114,23 +174,28 @@ class Interface(QMainWindow, Ui_MainWindow):
                          self.series_rating_checkbox.isChecked() or
                          self.mark_assumed_checkbox.isChecked())
         if not self.device_duty_checkbox.isChecked() and not self.arc_flash_checkbox.isChecked():
-            self.show_message('Input Error', 'Please select a study.')
+            self.show_message(self.INPUT_ERROR_TITLE, self.RUNTIME_ERROR_MSG)
             return False
         if not self.create_reports_checkbox.isChecked() and not self.create_scenarios_checkbox.isChecked():
-            self.show_message('Input Error', 'Please select an option to continue.')
+            self.show_message(self.INPUT_ERROR_TITLE, self.NO_OPTION_SELECTED_MSG)
             return False
         if port_required and (not port or not port.isnumeric() or len(port) < 5):
-            self.show_message('Data Validation Error', 'Please enter a valid ETAP Datahub port number.')
+            self.show_message(self.DATA_VALIDATION_ERROR_TITLE, self.INVALID_PORT_MSG)
             return False
         if self.etap_dir.isEnabled() and (not input_dir_path or not Path(input_dir_path).is_dir()):
-            self.show_message('Data Validation Error', 'Please enter a valid ETAP project directory path.')
+            self.show_message(self.DATA_VALIDATION_ERROR_TITLE, self.INVALID_ETAP_DIR_MSG)
             return False
         if self.output_dir.isEnabled() and (not output_dir_path or not Path(output_dir_path).is_dir()):
-            self.show_message('Data Validation Error', 'Please enter a valid output file directory path.')
+            self.show_message(self.DATA_VALIDATION_ERROR_TITLE, self.INVALID_OUTPUT_DIR_MSG)
             return False
         return True
 
-    def handle_options_toggle(self, is_checked):
+    def handle_options_toggle(self, is_checked: bool) -> None:
+        """
+        Toggles the visibility of the DataHub note based on option selections.
+
+        :param bool is_checked: Whether an option checkbox is checked.
+        """
         options_group = [
             self.series_rating_checkbox,
             self.mark_assumed_checkbox,
@@ -142,7 +207,10 @@ class Interface(QMainWindow, Ui_MainWindow):
         else:
             self.datahub_note.setVisible(True)
 
-    def add_connections(self):
+    def add_connections(self) -> None:
+        """
+        Adds connections for toggling options and setting default directories.
+        """
         self.create_scenarios_checkbox.toggled['bool'].connect(self.handle_options_toggle)
         self.run_scenarios_checkbox.toggled['bool'].connect(self.handle_options_toggle)
         self.mark_assumed_checkbox.toggled['bool'].connect(self.handle_options_toggle)
@@ -150,10 +218,22 @@ class Interface(QMainWindow, Ui_MainWindow):
         self.etap_dir_checkbox.clicked['bool'].connect(self.set_default_output_dir)
 
     @staticmethod
-    def split_tags(tags, delimiter=';'):
+    def split_tags(tags: str, delimiter: str = ';') -> list:
+        """
+        Splits tags based on the given delimiter and strips whitespace.
+
+        :param str tags: The string of tags to split.
+        :param str delimiter: The delimiter used to split the tags.
+        :return list: A list of cleaned tag strings.
+        """
         return [tag.strip() for tag in filter(None, tags.split(delimiter))]
 
-    def get_revisions(self):
+    def get_revisions(self) -> list | None:
+        """
+        Retrieves the revision information based on user selections.
+
+        :return list | None: A list of revisions or None if all revisions are included.
+        """
         if self.include_all_radio.isChecked():
             return None
         if self.include_base_radio.isChecked():
@@ -162,19 +242,22 @@ class Interface(QMainWindow, Ui_MainWindow):
             revisions = self.include_revisions_input.text()
             return self.split_tags(revisions)
 
-    def clear_all_inputs(self):
+    def clear_all_inputs(self) -> None:
+        """
+        Clears all input fields and resets checkboxes to their default state.
+        """
         checkboxes = [
             self.device_duty_checkbox,
             self.arc_flash_checkbox,
             self.create_scenarios_checkbox,
             self.run_scenarios_checkbox,
+            self.create_reports_checkbox,
             self.mark_assumed_checkbox,
             self.series_rating_checkbox,
             self.sw_checkbox,
             self.swgr_checkbox,
             self.etap_dir_checkbox
         ]
-
         lines = [
             self.etap_dir,
             self.output_dir,
